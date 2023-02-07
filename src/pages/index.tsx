e09fb08c3e9a0b256eadbf6de5e24a52d60c2c92
Robert Teets <teetsrobert@gmail.com>
2023-02-07 10:46:47 -0700

    feat: added framer-motion and added some basic animations to landing page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import { FormEvent } from "react";
+import { motion } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -34,18 +35,34 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <nav className="fixed top-0 right-0 left-0 z-50 flex h-16 list-none items-center justify-between px-10 text-2xl text-white">
+            <motion.nav
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                className="fixed top-0 right-0 left-0 z-50 flex h-16 list-none items-center justify-between px-10 text-2xl text-white"
+            >
                 <button>Pollgram</button>
                 <ul className="flex gap-5">
                     <li>Home</li>
                     <li>Your Polls</li>
                 </ul>
                 <button>Login</button>
-            </nav>
-            <main className="flex flex-col text-white backdrop-blur-lg">
+            </motion.nav>
+            <main className="flex flex-col overflow-x-hidden text-white backdrop-blur-lg">
                 <section className="flex h-screen items-end gap-16 p-5">
-                    <h1 className="flex-[3] text-[9rem]">Ask any question.</h1>
-                    <div className="flex h-[9rem] flex-[2] flex-col gap-3">
+                    <motion.h1
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 0.3, duration: 0.8 }}
+                        className="flex-[3] text-[9rem]"
+                    >
+                        Ask any question.
+                    </motion.h1>
+                    <motion.div
+                        initial={{ x: 800, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        transition={{ delay: 1, duration: 0.8 }}
+                        className="flex h-[9rem] flex-[2] flex-col gap-3"
+                    >
                         <p className="text-4xl">Ready to get started?</p>
                         <a
                             href="#createPoll"
@@ -54,7 +71,7 @@ export default function Home() {
                         >
                             Get Started
                         </a>
-                    </div>
+                    </motion.div>
                 </section>
 
                 <div className="h-[5rem] bg-gradient-to-b from-transparent to-[#00000080]"></div>
